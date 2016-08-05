91d5e8d2fc21b930933fa9b51a7edb32e7efdfb3
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2016-08-05 11:08:54 +0200

    Full memory check example

--- sources/memcheck/main.go
+++ sources/memcheck/main.go
@@ -1,18 +1,26 @@
+// START_IMPORT OMIT
 package main
 
 import (
 	"fmt"
 	"log"
-	"syscall"
+	"syscall" // HL
 )
 
+// START_STRUCT OMIT
 func main() {
-	var s syscall.Sysinfo_t
-	err := syscall.Sysinfo(&s)
+	//END_IMPORT OMIT
+	var s syscall.Sysinfo_t // HLSysinfo
+	// START_ERROR OMIT
+	err := syscall.Sysinfo(&s) // HLs
 
 	if err != nil {
 		log.Fatal(err)
 	}
+	// END_STRUCT OMIT
+	// END_ERROR OMIT
+	// START_DISPLAY OMIT
 	fmt.Println("Total ram:", s.Totalram)
 	fmt.Println("Free ram:", s.Freeram)
+	// END_DISPLAY OMIT
 }
